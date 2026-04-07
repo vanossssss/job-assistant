92508139c38f04ebad3dc355251e39c12393b9bd
vanossssss <kurdukov.434@mail.ru>
2026-04-07 14:06:29 +0500

    initial commit

---
+++ src/main/java/com/jobassistant/JobAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.jobassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobassistant/job_assistant/JobAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobassistant.job_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
